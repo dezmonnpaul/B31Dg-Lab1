8b16a6dc543e5b66fadc347434dc34e796b78eb8
LUNARWOLF94 <LUNARWOLF94@users.noreply.github.com>
2023-02-08 11:30:42 +0000

    UpdatedforTesting
    Renamed variables, improved comments and layout, removed Serial functions

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,3 +1,4 @@
+/*---------Definitions and Global Variables----------------------------------------------------------*/
 //Defining pins to the desired Input (In1,In2) and Outputs (SigA,SigB)
 #define SigA 15 //Signal A
 #define SigB 21 //Signal B
@@ -5,7 +6,10 @@
 #define In2 23 //Switch 2
 
 // Defning the initial counter value to determine the initial position of the waveform
-#define INITIAL 0 //
+#define INITIAL 0 
+
+//Creating a constant variable that can be used to increase the time multipler. 
+//This allows delays in microseconds to be delays in miliseconds if time_multi = 1000
 const int time_multi = 1000;
 
 //Defining parameters a,b,c and d for which the values are obtained as 1.1ms,0.1ms,10,6ms respectively.
@@ -20,16 +24,14 @@ int ctrl=  c;
 //Defining a variable that has a value of 0.05ms(50us)
 int comm_del = 0.05*time_multi;
 
-//'start' is a variable used as a counter of pulses which alos determines the start of a waveform when its value is 0
-int start = 0;
+//'Pulse_count' is a variable used as a counter of pulses which also 
+//determines the start of a waveform when its value is 0
+int pulse_count = 0;
 
 //'inc' is a variable that holds the incremental delay that would be added to 'a' after every pulse
 int inc = 0;
 
-
-//Creating a constant variable that can be used to increase the time multipler. This allows
-// delays in microseconds to be delays in miliseconds if time_multi = 1000
-
+/*------------------------------------------------------------------------------------------------------*/
 
 void setup() {
   //Seting up the Output and Input pins
@@ -37,53 +39,52 @@ void setup() {
   pinMode(SigB, OUTPUT);
   pinMode(In1, INPUT);
   pinMode(In2, INPUT);
-
-  Serial.begin(115200); //Setting up the serial monitor baud rate at 115200
 }
 
 void loop() {
-  if(start==INITIAL){//First checks if the current loop is at the initial position of the waveform
-    ctrl=c-3*(digitalRead(In2)); //Determines the total number of pulses to output based on the state of Switch 2
+  //First checks if the current loop is at the initial position of the waveform
+  if(pulse_count==INITIAL){
+    //Determines the total number of pulses to output based on the state of Switch 2
+    ctrl=c-3*(digitalRead(In2)); 
 
+    //Determines if Switch 1 is LOW and outputs Signal B if it is
+    if(!digitalRead(In1)){ 
+    //Output Signal B as High for 50us
+      digitalWrite(SigB, HIGH);
+      delay(comm_del); 
+      digitalWrite(SigB, LOW); 
 
-    if(!digitalRead(In1)){ //Determines if Switch 1 is LOW and outputs Signal B if it is
-      Serial.print("SignalB- High ");
-      digitalWrite(SigB, HIGH); //Output Signal B as High
-      delay(comm_del); // Delays for 50us(pusle width of signal B)
-      Serial.print(comm_del);
-      Serial.print(" SignalB-LOW");
-      Serial.print('\n');
-      digitalWrite(SigB, LOW); //Output of Signal B is LOW
-      start=start +1; //Increment start by 1 to allow the output of Signal A
+      //Increment pulse_count by 1 to allow the output of Signal A
+      pulse_count=pulse_count +1; 
     }
-    //If Switch 1 is HIGH, no there will be no output and start will remain as 0, thus disabling the output stream of pulses
+    //If Switch 1 is HIGH, no there will be no output and 
+    //pulse_count will remain as 0, thus disabling the output stream of pulses
   }
 
-  //Determines if the waveform has started and if the number of pulses output has not exceeded the maximum
-  else if (start>0 && start <ctrl+1) {
-    digitalWrite(SigA,HIGH); //Output Signal A as High
-    Serial.print(start);
-    Serial.print(" :SignalA- High ");
-    delay(a+inc); //Delays the change in output for a+inc ms (pulse width)
-    Serial.print(a+inc);
-    Serial.print(" SignalA- LOW");
-    Serial.print('\n');
-    digitalWrite(SigA,LOW); //Output Signal A as LOW
-    inc = inc + comm_del;//incrementing the value of inc by 50us, thus increaing the overall pulse width of the following pulse 
-    Serial.print("Delay: ");
-    Serial.print(b);
-    Serial.print('\n');
-    delay(b);// Delays the change in output for b ms
-    start=start+1; //Increments start by 1 to signify a pulse has been output
+  //Determines if the waveform has started and 
+  //if the number of pulses output has not exceeded the maximum
+  else if (pulse_count>0 && pulse_count <ctrl+1) {
+    //Output Signal A as high for a+inc ms
+    digitalWrite(SigA,HIGH);
+    delay(a+inc);
+    digitalWrite(SigA,LOW); 
+
+    //providing a delay of b ms before the next pulse
+    delay(b);
+
+    //incrementing the value of inc by 50us, and increasing the pusle counter 
+    inc = inc + comm_del;
+    pulse_count=pulse_count+1;
   }
+
   //Determines if the waveform has completed the cycle of pulses
   else { 
-    Serial.print("Delay: ");
-    Serial.print(d); 
-    Serial.print('\n');
     delay(d); //Provides a delay of d ms
-    inc=0; //resets the values of inc and start
-    start=0;
+
+    //resets the values of inc and pulse_count
+    inc=0; 
+    pulse_count=0;
   }
 
 }
+/*---------------------------------------End of code--------------------------------------------*/
